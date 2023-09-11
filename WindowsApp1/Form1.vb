7d42d7759b29cd344fa674cb592c055ff9e81f8f
abdelrahman992-cpu <abdelrahmandarrage2020@gmail.com>
2023-09-11 23:07:25 +0300

    Add project files.

---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim secret, read As Integer
+        Dim read1 As String
+        Randomize()
+        Do
+            secret = 1 + 5 * Rnd()
+            read1 = InputBox("Enter a number between 1 and 5 ")
+            read = Integer.Parse(read1)
+            If read = secret Then
+                MsgBox("Congratulation Your Guess is right .")
+            Else
+                MsgBox(Format(secret, "Your guess is wrong " & vbCrLf & "The right number is # ."))
+
+            End If
+
+        Loop While (read <> secret)
+    End Sub
+End Class
